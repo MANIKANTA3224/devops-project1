8ce64fec63ab4d8326c57711f330cf5f2157b16f
MANIKANTA3224 <manikt3224@gmail.com>
2025-12-12 13:31:37 +0530

    Refactor playbook and application files: update service to use Gunicorn, modify health check, and add Gunicorn to requirements

--- app/app.py
+++ app/app.py
@@ -1,13 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Deva DevOps Final  Project!"
-
-@app.route("/health")
-def health():
-    return {"status": "ok"}
+    return "Hello! Your CI/CD pipeline works perfectly 🎉"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run()
