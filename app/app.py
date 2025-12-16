f2ecc3832711ccfe6a2d83819863f5c62685fd63
MANIKANTA3224 <153202534+MANIKANTA3224@users.noreply.github.com>
2025-12-16 08:47:46 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! Your CI/CD pipeline works perfectly 🎉"
+    return "Hello! Your CI/CD pipeline works perfectly  🎉"
 
 if __name__ == "__main__":
     app.run()
