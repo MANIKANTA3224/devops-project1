0418cf4915a35707ebd7679994f71434aa04a8f2
MANIKANTA3224 <153202534+MANIKANTA3224@users.noreply.github.com>
2025-12-17 13:21:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,29 +30,23 @@ resource "aws_security_group" "web_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "web-sg"
+  }
 }
-
-# Key Pair
-resource "aws_key_pair" "main_key" {
-  key_name   = "github-actions-key"
-  public_key = file("${path.module}/../ansible/keys/id_rsa.pub")
-}
-
-
 
 # EC2 Instance
 resource "aws_instance" "web_server" {
   ami           = "ami-00d8fc944fb171e29"   # Ubuntu 22.04 ap-southeast-1
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.main_key.key_name
+
+  # IMPORTANT: Key created manually in AWS EC2 console
+  key_name = "devops-ci-key"
 
   vpc_security_group_ids = [
     aws_security_group.web_sg.id
   ]
-  depends_on = [
-    aws_key_pair.main_key
-  ]
-
 
   tags = {
     Name = "devops-web-server"
